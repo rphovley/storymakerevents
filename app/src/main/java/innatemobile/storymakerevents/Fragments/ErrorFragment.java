576f3abf5a49f3bc0451ccb33553c82e8683d6b2
Paul Hovley <rphovley@gmail.com>
2016-04-14 00:19:40 -0600

    Refactored Feedback Fragment

--- app/src/main/java/innatemobile/storymakerevents/Fragments/ErrorFragment.java
+++ app/src/main/java/innatemobile/storymakerevents/Fragments/ErrorFragment.java
@@ -10,8 +10,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-
 import innatemobile.storymakerevents.R;
 import innatemobile.storymakerevents.Utils.RequestSpreadsheets;
 
--- app/src/main/java/innatemobile/storymakerevents/Fragments/FeedbackFragment.java
+++ app/src/main/java/innatemobile/storymakerevents/Fragments/FeedbackFragment.java
@@ -5,11 +5,8 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.preference.PreferenceManager;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.SwitchCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,16 +16,13 @@ import android.widget.TextView;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
-import innatemobile.storymakerevents.Activities.MainActivity;
 import innatemobile.storymakerevents.Models.Presentations;
 import innatemobile.storymakerevents.Models.Spreadsheets;
 import innatemobile.storymakerevents.R;
-import innatemobile.storymakerevents.Utils.AppController;
 import innatemobile.storymakerevents.Utils.DatabaseHandler;
-import innatemobile.storymakerevents.Utils.ParseJSON;
 
 /**
- * A simple {@link Fragment} subclass.
+ *Feedback fragment displays buttons to link to the google feedback forms
  */
 public class FeedbackFragment extends Fragment implements View.OnClickListener {
 
@@ -37,28 +31,14 @@ public class FeedbackFragment extends Fragment implements View.OnClickListener {
         // Required empty public constructor
     }
     SharedPreferences prefs;
-    TextView txtSubText, txtCourse, txtConference;
+    TextView txtCourse, txtConference;
+    View view;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_feedback, container, false);
-        //android.support.v7.widget.SwitchCompat toggle = (SwitchCompat) view.findViewById(R.id.toggleFeedback);
-        //txtSubText    = (TextView) view.findViewById(R.id.txtFeedbackSub);
-        txtCourse     = (TextView) view.findViewById(R.id.txtCourseFeedback);
-        txtConference = (TextView) view.findViewById(R.id.txtConferenceFeedback);
-        prefs = PreferenceManager
-                .getDefaultSharedPreferences(getActivity());
-        /*if(!prefs.getBoolean(MainActivity.TOGGLE_NOTIFICATIONS, false)) {
-            toggle.setChecked(false);
-            txtSubText.setText("Notifications for feedback reminders are off");
-        }else{
-            toggle.setChecked(true);
-            txtSubText.setText("Notifications for feedback reminders are on");
-        }*/
-        //toggle.setOnClickListener(this);
-        txtConference.setOnClickListener(this);
-        txtCourse.setOnClickListener(this);
+        view = inflater.inflate(R.layout.fragment_feedback, container, false);
+        bindViews();
         return view;
     }
 
@@ -82,15 +62,41 @@ public class FeedbackFragment extends Fragment implements View.OnClickListener {
                 }
                 break;*/
             case R.id.txtCourseFeedback:
-                openBrowser(Spreadsheets.COURSE_SHEET, 1);
+                googleFeedback(Spreadsheets.COURSE_SHEET, 1);
                 break;
             case R.id.txtConferenceFeedback:
-                openBrowser(Spreadsheets.CONFERENCE_SHEET, -1);
+                googleFeedback(Spreadsheets.CONFERENCE_SHEET, -1);
                 break;
         }
     }
 
-    public void openBrowser(String spreadsheetName, int pres_id){
+    /**
+     * Binds the data to the views
+     * currently the toggle to turn notifications on and off is commented out until the functionality is needed
+     * */
+    public void bindViews(){
+        //android.support.v7.widget.SwitchCompat toggle = (SwitchCompat) view.findViewById(R.id.toggleFeedback);
+        //txtSubText    = (TextView) view.findViewById(R.id.txtFeedbackSub);
+        txtCourse     = (TextView) view.findViewById(R.id.txtCourseFeedback);
+        txtConference = (TextView) view.findViewById(R.id.txtConferenceFeedback);
+        prefs = PreferenceManager
+                .getDefaultSharedPreferences(getActivity());
+        /*if(!prefs.getBoolean(MainActivity.TOGGLE_NOTIFICATIONS, false)) {
+            toggle.setChecked(false);
+            txtSubText.setText("Notifications for feedback reminders are off");
+        }else{
+            toggle.setChecked(true);
+            txtSubText.setText("Notifications for feedback reminders are on");
+        }*/
+        //toggle.setOnClickListener(this);
+        txtConference.setOnClickListener(this);
+        txtCourse.setOnClickListener(this);
+    }
+
+    /**
+     * Opens google feedback forms
+     * */
+    public void googleFeedback(String spreadsheetName, int pres_id){
         DatabaseHandler dh = new DatabaseHandler(getContext());
         String url = dh.getSpreadsheetLink(spreadsheetName);
         if(pres_id != -1){
